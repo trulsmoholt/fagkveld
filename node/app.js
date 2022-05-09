b7556a58cdd071725b6550248f7279c5d7cf508a
trulsmholt <truls.moholt@visma.com>
2022-05-09 13:49:28 +0200

    Enkelt oppsett

---
+++ node/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('<p>Hello World!</p>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
