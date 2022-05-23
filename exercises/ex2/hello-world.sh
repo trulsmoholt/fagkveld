25a2adc4341c5a451b60cdef3b10119484546949
trulsmholt <truls.moholt@visma.com>
2022-05-23 09:30:13 +0200

    ferdig med fagkveld

---
+++ exercises/ex2/hello-world.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+var="hello world!"
+
+echo "$var"
--- exercises/ex3/index.js
+++ exercises/ex3/index.js
@@ -1,17 +1,10 @@
 const express = require('express')
-const cors = require('cors')
-
 const app = express()
-const port = 80
-
-app.use(cors({
-  origin: '*'
-}))
+const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('LALALAA')
+  res.send('Hello World!')
 })
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
